10ce54b59d4d1cc573666d8dd01169da9c5daf76
khinoj <kristopher.hinojosa@gmail.com>
2023-08-08 22:19:27 -0400

    cleaned up html, js priting next days and css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,11 @@ let day1TemperatureEl = document.querySelector('.day1Temperature');
 let day1HumidityEl = document.querySelector('.day1Humidity');
 let day1WindEl = document.querySelector('.day1Wind');
 let day1WeatherEl = document.querySelector('.day1Weather');
-let box1El = document.getElementById('box1');
-let box2El = document.getElementById('box2');
-let box3El = document.getElementById('box3');
-let box4El = document.getElementById('box4');
-let box5El = document.getElementById('box5');
+let fiveDaysForecastEl = document.getElementById('fiveDaysForecast')
 const date = new Date();
 const todaysDate = date.toLocaleDateString();
 
-
+// shows the daily section upon button click with userinput
 function todayWeather() {
     searchbtnEl.addEventListener('click', function () {
         console.log('Search BTN');
@@ -39,6 +35,7 @@ function todayWeather() {
                 const lat = forecastResponse.coord.lat;
                 const lon = forecastResponse.coord.lon;
 
+                //creates a span and takes data into text.
                 let cityName = document.createElement('span');
                 cityName.textContent = (forecastResponse.name) + " - " + todaysDate;
                 day1CityEl.appendChild(cityName);
@@ -55,6 +52,7 @@ function todayWeather() {
                 cityWind.textContent = (forecastResponse.wind.speed) + " " + 'MPH';
                 day1WindEl.appendChild(cityWind);
 
+                //checks to see what the weather is and put the correct icon
                 if (forecastResponse.weather[0].main === 'Clear') {
                     let cityWeather = document.createElement('span');
                     cityWeather.textContent = (forecastResponse.weather[0].main) + " " + " ⛅ "
@@ -83,6 +81,7 @@ function todayWeather() {
 
 todayWeather()
 
+//clears the daily section
 function clearWeather() {
     day1CityEl.textContent = " ";
     day1TemperatureEl.textContent = " ";
@@ -91,8 +90,10 @@ function clearWeather() {
     day1WeatherEl.textContent = " ";
 };
 
+//Next Day Weather Report
 function fiveDays(lat, lon) {
     let requesturl2 = `http://api.openweathermap.org/data/2.5/forecast?q=${$('#userinput').val()}&lat=${lat}&lon=${lon}&appid=${API_Key}&units=imperial`;
+
     fetch(requesturl2)
         .then(function (response) {
             return response.json();
@@ -100,11 +101,37 @@ function fiveDays(lat, lon) {
         .then(function (fiveData) {
             console.log('five Day', fiveData.list);
 
-            for (let i = 0; i < fiveData.list.length; i+=8) {
-                console.log('i', fiveData.list[i]);
-             
-                
+            // Clear existing content inside the fiveDaysForecastEl element
+            fiveDaysForecastEl.innerHTML = '';
 
-            };
-        })
+            for (let i = 0; i < fiveData.list.length; i += 8) {
+                const forecast = fiveData.list[i];
+
+                // Create a div element to hold each day's forecast
+                const dayForecast = document.createElement('div');
+                dayForecast.classList.add('day-forecast'); 
+
+                // Create elements to display forecast details
+                const dateEl = document.createElement('h3');
+                dateEl.textContent = forecast.dt_txt.trim(); 
+
+                const temperatureEl = document.createElement('p');
+                temperatureEl.textContent = 'Temperature: ' + forecast.main.temp + ' F';
+
+                const humidityEl = document.createElement('p');
+                humidityEl.textContent = 'Humidity: ' + forecast.main.humidity + '%';
+
+                // Append the elements to the dayForecast div
+                dayForecast.appendChild(dateEl);
+                dayForecast.appendChild(temperatureEl);
+                dayForecast.appendChild(humidityEl);
+
+                // Append the dayForecast div to the fiveDaysForecastEl element
+                fiveDaysForecastEl.appendChild(dayForecast);
+            }
+        });
 }
+
+
+
+
